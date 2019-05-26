3c83ffc9a2df8e7a03b9a012bc44803dec55fa63
Paulemberg Lima Silva <paulemberg@gmail.com>
2019-05-26 16:31:46 -0300

    Add project files.

---
+++ MyAccount/DAL/ContaDAL.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MyAccount.DAL
+{
+    public class ContaDAL 
+    {
+        private string connectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FinancesDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+        public IEnumerable<Conta>
+
+
+    }
+}
---
+++ MyAccount/Models/ContaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAccount.Models
+{
+    public class ContaModel
+    {
+        public int Id { get; set; }
+        public double Saldo { get; set; }   
+    }
+}
